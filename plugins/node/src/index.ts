e45766911ce64343ff7afdd52e327d5d3317fec7
KTrain5369 <me@git.ktrain5369.dev>
2026-09-18 19:32:51 +1000

    remove ssr boolean from environment

--- plugins/node/src/index.ts
+++ plugins/node/src/index.ts
@@ -127,7 +127,6 @@ export function node(opts: PluginOptions): Plugin {
               },
             },
             build: {
-              ssr: true,
               outDir: `dist/${environmentName}`,
 
               rolldownOptions: {
